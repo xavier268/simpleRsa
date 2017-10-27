33a636fbc86c0499d0456a803f87803efd0701ae
Xavier(fedora26.vbox) <xavier@nospamplease>
2017-10-27 10:41:58 +0200

    Fixed digest bug.

--- src/main/java/com/twiceagain/simplersa/Key.java
+++ src/main/java/com/twiceagain/simplersa/Key.java
@@ -13,6 +13,7 @@ import java.util.logging.Logger;
 
 /**
  * Abstract class, common to public and private keys.
+ *
  * @author xavier
  */
 public abstract class Key {
@@ -20,22 +21,30 @@ public abstract class Key {
     protected BigInteger exponent;
     protected BigInteger pubKey;
 
+    public static final String DIGEST_ALGO = "SHA-256";
+
     private MessageDigest md;
 
     {
         try {
-            md = MessageDigest.getInstance("SHA-256");
+            md = MessageDigest.getInstance(DIGEST_ALGO);
         } catch (NoSuchAlgorithmException ex) {
+            // Should never be thrown, unless jvm does not provides message digest.
+            System.out.printf("\nYour jvm implementation is not providing "
+                    + DIGEST_ALGO + " message digest ...\n");
             Logger.getLogger(Key.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
     /**
      * Compute the hash as a BigInteger modulus.
+     *
      * @param message
-     * @return 
+     * @return
      */
     protected BigInteger digest(String message) {
-        return new BigInteger(message.getBytes()).mod(pubKey);
+        byte[] digest = md.digest(message.getBytes());
+        BigInteger bd = new BigInteger(digest);
+        return bd.mod(pubKey);
     }
 }
